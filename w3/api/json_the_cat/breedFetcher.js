49a6823e9967afcd3eb9034c817f310f719c4b73
pochiman <pochiman@yahoo.com>
2021-11-29 08:36:27 -0500

    initial commit

--- w3/api/json_the_cat/breedFetcher.js
+++ w3/api/json_the_cat/breedFetcher.js
@@ -1,1 +1,18 @@
+const request = require("request");
 
+const fetchBreedDescription = function(breedname, callback) {
+
+  const link = `https://api.thecatapi.com/v1/breeds/search?q=${breedname}`;
+
+  request(link, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (data[0]) {
+      return callback(error, data[0].description.trim());
+    } else {
+      return callback("Unable to find the breed name!");
+    }
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
